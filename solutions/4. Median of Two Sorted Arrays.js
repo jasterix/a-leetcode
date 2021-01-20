0d341cfc0afe7e32750e83286f9a6db8fe06d55c
Jasterix <jasterix@users.noreply.github.com>
2021-01-20 15:07:29 -0500

    148 ms

--- solutions/4. Median of Two Sorted Arrays.js
+++ solutions/4. Median of Two Sorted Arrays.js
@@ -4,5 +4,16 @@
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
+    let both = [...nums1, ...nums2].sort((a,b)=> a-b)
     
+    let res = 0
+    let med = Math.floor(both.length/2)
+    
+    if (both.length%2 === 0){
+        res = (both[med-1]+ both[med])/2
+    } else {
+        res = both[med]
+    }
+    
+    return res
 };
